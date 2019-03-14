0e95023cbc6a8aeb1187d686395cd8f110eee6f7
Kuba Żeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2019-03-14 16:08:27 +0100

    Befor lesson

--- server/server-test.js
+++ server/server-test.js
@@ -2,47 +2,51 @@ const WebSocket = require('ws');
 const env = require('./env.js');
 //const msgpack = require('msgpack-lite');
 
-// const angToLin = (velocity) => {
-//   x: Math.sin(velocity.angle)*velocity.val,
-//   y: Math.cos(velocity.angle)*velocity.val
-// }
-
-
-
 function rotate(velocity, angle) {
-  const rotatedVelocities = {
-    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-  };
+    const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
 
-  return rotatedVelocities
+    return rotatedVelocities;
 }
 
 function resolveCollision(particle, otherParticle) {
-  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
 
-  // Grab angle between the two colliding particles
-  const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    const xDist = otherParticle.x - particle.x;
+    const yDist = otherParticle.y - particle.y;
 
-  // Velocity before equation
-  const u1 = rotate(particle.velocity, angle);
-  const u2 = rotate(otherParticle.velocity, angle);
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
 
-  // Velocity after 1d collision equation
-  const v1 = { x: u2.x, y: u1.y };
-  const v2 = { x: u1.x, y: u2.y };
+        // Grab angle between the two colliding particles
+        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
 
-  // Final velocity after rotating axis back to original location
-  const vFinal1 = rotate(v1, -angle);
-  const vFinal2 = rotate(v2, -angle);
+        // Store mass in var for better readability in collision equation
+        const m1 = particle.mass;
+        const m2 = otherParticle.mass;
 
-  // Swap particle velocities for realistic bounce effect
-  particle.velocity.x = vFinal1.x;
-  particle.velocity.y = vFinal1.y;
+        // Velocity before equation
+        const u1 = rotate(particle.velocity, angle);
+        const u2 = rotate(otherParticle.velocity, angle);
 
-  otherParticle.velocity.x = vFinal2.x;
-  otherParticle.velocity.y = vFinal2.y;
+        // Velocity after 1d collision equation
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherParticle.velocity.x = vFinal2.x;
+        otherParticle.velocity.y = vFinal2.y;
+    }
 }
 
 const server = new WebSocket.Server({
@@ -54,7 +58,7 @@ let players = {}
 setInterval(()=>{
   for(var id in players) {
     if(!players[id].spawned) continue;
-    players[id].calcAxis()
+    players[id].calcAcceleration()
     players[id].calcV()
     players[id].move()
     players[id].send({
@@ -75,15 +79,22 @@ setInterval(()=>{
 
 class Player {
   constructor(ws, id) {
-    this.velocity = {}
-    this.mass = 1
-    this.id = id;
-    this.ws = ws;
-    this.r = 14
+    this.acceleration = {
+      x: 0,
+      y: 0
+    }
     this.moveAxis = {
       x: 0,
       y: 0
     }
+    this.velocity = {
+      val: 0,
+      angle: 0
+    }
+    this.mass = 1
+    this.id = id;
+    this.ws = ws;
+    this.r = 14
     this.x = this.y = 0
     this.uHeld = this.dHeld = this.lHeld = this.rHeld = false
   }
@@ -96,29 +107,45 @@ class Player {
   calcV() {
     this.v = Math.sqrt(this.moveAxis.x ** 2 + this.moveAxis.y ** 2)
   }
-  calcAxis() {
-    this.moveAxis.x *= (11 / 12)
-    this.moveAxis.y *= (11 / 12)
+  calcAcceleration() {
+    this.acceleration.x = 0
+    this.acceleration.y = 0
     if(this.rHeld === true) {
-      this.moveAxis.x = Math.min(this.moveAxis.x + 10, 60)
+      this.acceleration.x = 10
     }
     if(this.lHeld === true) {
-      this.moveAxis.x = Math.max(this.moveAxis.x - 10, -60)
+      this.acceleration.x = -10
     }
     if(this.uHeld === true) {
-      this.moveAxis.y = Math.min(this.moveAxis.y + 10, 60)
+      this.acceleration.y = 10
     }
     if(this.dHeld === true) {
-      this.moveAxis.y = Math.max(this.moveAxis.y - 10, -60)
+      this.acceleration.y = -10
     }
   }
+
+  calcVelocity() {
+
+  }
+
+  doMovement() {
+    const possiblePos = {
+      x:
+    }
+    // Colision with players
+
+    // Colision with walls
+    // Move player
+
+  }
+
+  /////////
+
   distance(player) {
     return Math.sqrt((this.x - player.x)**2 + (this.y - player.y)**2)
   }
 
-  calcVelocity() {
-    this.velocity.val = Math.sqrt(this.moveAxis.x ** 2 + this.moveAxis.y ** 2)
-  }
+
 
   move() {
     let angle = Math.atan2(this.moveAxis.y, this.moveAxis.x)
